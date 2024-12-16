ffe245d580af04c3697d4d18c3cd3e79c3edaf53
Anil Rumdali Rai <anil.rai704757@gmail.com>
2024-12-16 19:28:20 +0545

    Message-Box Added

--- client/src/components/chat/Chat.jsx
+++ client/src/components/chat/Chat.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "./chat.scss";
 import Anil from "../../assets/anil.jpg";
 
 const Chat = () => {
+  const [chat,setChat] = useState(true)
   return (
     <div className="chat">
       <div className="messages">
@@ -33,7 +34,54 @@ const Chat = () => {
           <p>Lorem ipsum dolor sit amet...</p>
         </div>
       </div>
-      <div className="chatBox">box</div>
+      {chat && ( 
+        <div className="chatBox">
+        <div className="top">
+          <div className="user">
+            <img src={Anil} alt="" />
+            Anil Rai
+          </div>
+          <span className="close" onClick={()=>setChat(null)}>X</span>
+        </div>
+        <div className="center">
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+          <div className="chatMessage own">
+            <p>Lorem ipsum dolor sit amet.</p>
+            <span>1 hour ago</span>
+          </div>
+        </div>
+        <div className="bottom">
+          <textarea></textarea>
+          <button>Send</button>
+        </div>
+      </div>)}
     </div>
   );
 };
