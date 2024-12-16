48a42bed5f48be26eb9c3917bb77adb18d0e6065
Anil Rumdali Rai <anil.rai704757@gmail.com>
2024-12-16 10:54:37 +0545

    Map Added

--- client/src/routes/listPage/ListPage.jsx
+++ client/src/routes/listPage/ListPage.jsx
@@ -3,6 +3,7 @@ import "./listpage.scss";
 import { listData } from "../../lib/dummydata";
 import { SearchFilter } from "../../components/searchFilter/SearchFilter";
 import { Card } from "../../components/card/Card";
+import Map from "../../components/map/Map";
 const ListPage = () => {
   const data = listData;
   return (
@@ -15,7 +16,9 @@ const ListPage = () => {
           ))}
         </div>
       </div>
-      <div className="mapContainer">Map</div>
+      <div className="mapContainer">
+        <Map items={data} />
+      </div>
     </div>
   );
 };
